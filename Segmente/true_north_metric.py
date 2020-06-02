0c7a78aa884ef795bfe3a238d5b480cc626f2c9c
Kimaru thagana <macbuntu@localhost.localdomain>
2020-06-02 14:22:16 +0300

    generate order count and customer count

--- Segmente/true_north_metric.py
+++ Segmente/true_north_metric.py
@@ -19,6 +19,7 @@ transaction_data['InvoiceYearMonth'] = transaction_data['InvoiceDate'].map(lambd
 transaction_data['Revenue'] = transaction_data['UnitPrice'] * transaction_data['Quantity']
 # generate revenue dataframe
 transaction_revenue = transaction_data.groupby(['InvoiceYearMonth'])['Revenue'].sum().reset_index()
+transaction_revenue['InvoiceYearMonth'] = transaction_revenue['InvoiceYearMonth'].astype('category')
 #print(transaction_revenue.head())
 # monthly growth rate
 
@@ -27,10 +28,40 @@ sns.lineplot(x=transaction_revenue['InvoiceYearMonth'],
              y=transaction_revenue['Revenue'].apply(lambda revenue: revenue/1000000), data=transaction_revenue)
 plt.title('Monthly Revenue in GBP')
 plt.ylabel('Revenue in Millions (M)')
+plt.show()
 
 transaction_revenue['MonthlyGrowthRate'] = transaction_revenue['Revenue'].pct_change()
+
 sns.lineplot(x=transaction_revenue['InvoiceYearMonth'],
              y=transaction_revenue['MonthlyGrowthRate'], data=transaction_revenue)
 plt.title('Monthly Revenue Growth ')
 plt.ylabel('Revenue change (%)')
+plt.show()
+
+# monthly active users
+uk_users = transaction_data.query("Country=='United Kingdom'").reset_index(drop=True)
+#creating monthly active customers dataframe by counting unique Customer IDs nunique returns number of distinct observations
+monthly_active_uk_users = uk_users.groupby('InvoiceYearMonth')['CustomerID'].nunique().reset_index()
+print(monthly_active_uk_users.head())
+
+sns.barplot(x=monthly_active_uk_users['InvoiceYearMonth'],
+             y=monthly_active_uk_users['CustomerID'], data=monthly_active_uk_users)
+plt.title('Monthly Active Users ')
+plt.ylabel('Number of Active Users')
+plt.show()
+
+# monthly order count
+transaction_orders = transaction_data.groupby(['InvoiceYearMonth'])['Quantity'].sum().reset_index()
+sns.barplot(x=transaction_orders['InvoiceYearMonth'],
+             y=transaction_orders['Quantity'], data=transaction_orders)
+plt.title('Monthly Orders')
+plt.ylabel('Number of orders made')
+plt.show()
+
+# average revenue per month
+transaction_avg_revenue = transaction_data.groupby(['InvoiceYearMonth'])['Revenue'].mean().reset_index()
+sns.barplot(x=transaction_avg_revenue['InvoiceYearMonth'],
+             y=transaction_avg_revenue['Revenue'], data=transaction_avg_revenue)
+plt.title('Monthly Orders Average Revenue')
+plt.ylabel('Average Revenue')
 plt.show()
