ef8c06fa83d080e35baadf5e4f84ec0c4e082c86
Kimaru thagana <macbuntu@localhost.localdomain>
2020-06-10 17:14:11 +0300

    use barplot to visualize distribution of gender with regards to churn

--- Segmente/churn_predict.py
+++ Segmente/churn_predict.py
@@ -8,3 +8,14 @@ import xgboost as xgb
 
 churn_data = pd.read_csv('datasets/telco_data.csv')
 print(churn_data.head())
+churn_data.loc[churn_data.Churn=='No','Churn'] = 0
+churn_data.loc[churn_data.Churn=='Yes','Churn'] = 1
+
+# visualize gender distribution of the churn rate
+gender_plot = churn_data.groupby('gender').Churn.mean().reset_index()
+sns.barplot(x=gender_plot['gender'], y=gender_plot['Churn'], data=gender_plot)
+sns.despine(left=True, bottom=True)
+plt.title('Gender Distribution of Churn')
+plt.ylabel('Number of Churns')
+plt.xlabel('Gender')
+plt.show()
