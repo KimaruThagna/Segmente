286a42ad0f069d7dfdb687a78c46e98bbd0b3a0e
Kimaru thagana <macbuntu@localhost.localdomain>
2020-06-13 17:03:09 +0300

    add churn probability

--- Segmente/churn_predict.py
+++ Segmente/churn_predict.py
@@ -155,3 +155,5 @@ print(classification_report(y_test, y_pred))
 # find out important features
 fig, ax = plt.subplots(figsize=(10,8))
 xgb.plot_importance(xgb_model, ax=ax)
+# add probability column
+churn_data['proba'] = xgb_model.predict_proba(churn_data[X_train.columns])[:,1]
