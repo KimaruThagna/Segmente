01e4c35626aadc0111bc78313e4dc9669df8c326
Kimaru thagana <macbuntu@localhost.localdomain>
2020-06-20 21:07:27 +0300

    plot monthly sales data

--- Segmente/predict_purchase.py
+++ Segmente/predict_purchase.py
@@ -14,10 +14,26 @@ from keras.layers import LSTM
 from sklearn.model_selection import KFold, cross_val_score, train_test_split
 
 #read the data in csv
-df_sales = pd.read_csv('datasets/retail_sales.csv')
+sales = pd.read_csv('datasets/retail_sales.csv')
 
 #convert date field from string to datetime
-df_sales['date'] = pd.to_datetime(df_sales['date'])
+sales['date'] = pd.to_datetime(sales['date'])
 
 #show first 10 rows
-print(df_sales.head(10))
+print(sales.head(10))
+
+#represent month in date field as its first day
+sales['date'] = sales['date'].dt.year.astype('str') + '-' + sales['date'].dt.month.astype('str') + '-01'
+sales['date'] = pd.to_datetime(sales['date'])
+#groupby date and sum the sales
+sales = sales.groupby('date').sales.sum().reset_index()
+print('Total monthly Sales')
+print(sales.head())
+
+# plot monthly sales data
+sns.lineplot(x=sales['date'], y=sales['sales'], data=sales, dashes=True)
+sns.despine(left=True, bottom=True)
+plt.title('Monthly Sales data')
+plt.ylabel('Sales($)')
+plt.xlabel('Month')
+plt.show()
